82dcc314b8d6b59434d881efc5b86b8815eb42e7
Danilo ribeiro <danilo.ribeiro@noverde.com.br>
2020-05-08 17:30:06 -0300

    intial load

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'poc-react-library'
 import 'poc-react-library/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text=" XXXXCreate React Library Example 😄" />
 }
 
 export default App
