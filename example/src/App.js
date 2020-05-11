3cbc8858b2c0a7e8839f508b613b0d3faff38977
Danilo ribeiro <danilo.ribeiro@noverde.com.br>
2020-05-11 15:02:47 -0300

    add cep feature

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import { ExampleComponent } from 'poc-react-library'
+import pocLib from 'poc-react-library'
 import 'poc-react-library/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text=" XXXXCreate React Library Example 😄" />
+  const [cep, setCEP] = useState('');
+  const [finalCEP, setFinalCEP] = useState('');
+  
+  return <div>
+    <input type="text" value={cep} onBlur={e => setFinalCEP(e.target.value)} onChange={e => setCEP(e.target.value)}/>
+    <pocLib.ExampleComponent cep={finalCEP} /> 
+  </div>
 }
 
 export default App
--- src/example.jsx
+++ src/example.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component YYY: {text}</div>
+
+export const ExampleComponent = ({ cep }) => {
+  const [oldCEP, setOldCEP] = useState({});
+  const [address, setAddress] = useState({});
+  if (cep && cep !== oldCEP) {
+    setOldCEP(cep)
+    fetch(`https://api.postmon.com.br/v1/cep/${cep}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setAddress(result);
+        },
+        (error) => { 
+          setAddress({logradouro: 'não encontrado'});
+        }
+      )
+  }
+  return <div className={styles.test}>
+    {address.logradouro || ''}
+     - {address.bairro || ''}
+     - {address.cidade || ''}
+     - {address.estado || ''}
+  </div>
 }
